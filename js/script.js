e04471d984bace31a61ce3497159237f1d4b4e9b
edmilton <edmilton7500@gmail.com>
2023-01-25 23:27:20 -0300

    Primeiro commit

--- js/script.js
+++ js/script.js
@@ -5,8 +5,8 @@ import initTabNav from "./modules/tabnav.js";
 import initModal from "./modules/modal.js";
 import initTooltip from "./modules/tooltip.js";
 import initDropdownMenu from "./modules/dropdown-menu.js";
-import initMenuMobile from "./modules/menu-mobile.js"
-import initFuncionamento from "./modules/funcionamento.js"
+import initMenuMobile from "./modules/menu-mobile.js";
+import initFuncionamento from "./modules/funcionamento.js";
 import initFetchAnimais from "./modules/fetch-animais.js";
 import initFetchBitcoin from "./modules/fetch-bitcoin.js";
 
@@ -21,3 +21,5 @@ initMenuMobile();
 initFuncionamento();
 initFetchAnimais();
 initFetchBitcoin();
+
+console.log("object");
