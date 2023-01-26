a7ec10203fc55cb68e3a2f95d254b0480f2ddbcc
edmilton <edmilton7500@gmail.com>
2023-01-26 01:45:11 -0300

    SrollSuave transformado em class

--- js/modules/scroll-suave.js
+++ js/modules/scroll-suave.js
@@ -1,17 +1,31 @@
-export default function initScrollSuave() {
-    const linksInternos = document.querySelectorAll("[data-menu='suave'] a[href^='#']");
-  
-    function scrollToSection(event) {
-      event.preventDefault();
-      const href = event.currentTarget.getAttribute("href");
-      const section = document.querySelector(href);
-      section.scrollIntoView({
-        behavior: "smooth",
-        block: "start",
-      });
+export default class ScrollSuave {
+  constructor(links, options) {
+    this.linksInternos = document.querySelectorAll(links);
+    if (options === undefined) {
+      this.options = { behavior: "smooth", block: "start" };
+    } else {
+      this.options = options;
     }
-  
-    linksInternos.forEach((link) => {
-      link.addEventListener("click", scrollToSection);
+    this.scrollToSection = this.scrollToSection.bind(this);
+  }
+
+  scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute("href");
+    const section = document.querySelector(href);
+    section.scrollIntoView(this.options);
+  }
+
+  addLinkEvent() {
+    this.linksInternos.forEach((link) => {
+      link.addEventListener("click", this.scrollToSection);
     });
   }
+
+  init() {
+    if (this.linksInternos.length) {
+      this.addLinkEvent();
+    }
+    return this;
+  }
+}
