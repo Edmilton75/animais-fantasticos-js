83ceca42e01c29867e0f7dbe9b7b7d975ac59067
edmilton <edmilton7500@gmail.com>
2023-02-01 02:16:39 -0300

    refatoração com class

--- js/modules/scroll-animacao.js
+++ js/modules/scroll-animacao.js
@@ -1,20 +1,25 @@
-export default function initAnimacaoScroll() {
-  const sections = document.querySelectorAll("[data-anime='scroll']");
-  if (sections.length) {
-    const windowMetade = window.innerHeight * 0.6;
+export default class ScrollAnima {
+  constructor(sections) {
+    this.sections = document.querySelectorAll(sections);
+    this.windowMetade = window.innerHeight * 0.6;
 
-    function animaScroll() {
-      sections.forEach((section) => {
-        const sectionTop = section.getBoundingClientRect().top;
-        const isSectionVisible = sectionTop - windowMetade < 0;
-        if (isSectionVisible) {
-          section.classList.add("ativo");
-        } else if (section.classList.contains("ativo")) {
-          section.classList.remove("ativo");
-        }
-      });
-    }
-    animaScroll();
-    window.addEventListener("scroll", animaScroll);
+    this.animaScroll = this.animaScroll.bind(this);
+  }
+
+  animaScroll() {
+    this.sections.forEach((section) => {
+      const sectionTop = section.getBoundingClientRect().top;
+      const isSectionVisible = sectionTop - this.windowMetade < 0;
+      if (isSectionVisible) {
+        section.classList.add("ativo");
+      } else if (section.classList.contains("ativo")) {
+        section.classList.remove("ativo");
+      }
+    });
+  }
+
+  init() {
+    this.animaScroll();
+    window.addEventListener("scroll", this.animaScroll);
   }
 }
